f6331c54d5991f34a3c60130bbf2466bcded990d
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-06-11 20:38:03 -0600

    feat: support game version 1.511.102.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,14 @@ namespace Hooks
 	{
 		static bool QuitGame()
 		{
-			static REL::Relocation<void (*)(const char*, ...)> ConsolePrint{ REL::Offset(0x6593FA0) };
+			static REL::Relocation<void (*)(const char*, ...)> ConsolePrint{ REL::Offset(0x6568220) };
 			ConsolePrint("Bye.");
 
 			std::thread(
 				[]()
 				{
 					std::this_thread::sleep_for(std::chrono::milliseconds(200));
-					static REL::Relocation<void (*)(void*, const wchar_t*)> USendMessage{ REL::Offset(0x0F5EE40) };
-					USendMessage(nullptr, L"UGameEngine::HandleExitCommand");
+					REX::W32::TerminateProcess(REX::W32::GetCurrentProcess(), EXIT_SUCCESS);
 				})
 				.detach();
 
@@ -22,7 +21,7 @@ namespace Hooks
 
 	static void Install()
 	{
-		static REL::Relocation target{ REL::Offset(0x6A3CA50) };
+		static REL::Relocation target{ REL::Offset(0x6A10DE0) };
 		target.replace_func(0x21, QuitGame);
 	}
 }
